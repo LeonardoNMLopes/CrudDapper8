f69f100b801d2d071cc54097651f5dfdf20cadd0
JoséLopes <JosLopes@BIZAPP-0111>
2024-09-18 10:13:34 -0300

    Adicionar arquivos de projeto.

---
+++ CrudDapper8/Profiles/ProfileAutoMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CrudDapper8.Dto;
+using CrudDapper8.Models;
+
+namespace CrudDapper8.Profiles
+{
+    public class ProfileAutoMapper : Profile
+    {
+
+        public ProfileAutoMapper()
+        {
+            CreateMap<Usuario, UsuarioListarDto>();
+        }
+
+    }
+}
---
+++ CrudDapper8/Services/IUsuarioInterface.cs
@@ -0,0 +1,16 @@
+﻿using CrudDapper8.Dto;
+using CrudDapper8.Models;
+
+namespace CrudDapper8.Services
+{
+    public interface IUsuarioInterface
+    {
+        Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios();
+        Task<ResponseModel<UsuarioListarDto>>BuscarUsuarioPorId(int usuarioId);
+        Task<ResponseModel<List<UsuarioListarDto>>> CriarUsuario(UsuarioCriarDto usuarioCriarDto);
+        Task<ResponseModel<List<UsuarioListarDto>>> EditarUsuario(UsuarioEditarDto usuarioEditarDto);
+        Task<ResponseModel<List<UsuarioListarDto>>> RemoverUsuario(int usuarioId);
+
+    }
+}
+    
